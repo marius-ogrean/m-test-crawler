3916abcce3974c0ae14d934abb951cd364e64b74
Marius <marius_ogrean@yahoo.com>
2026-03-27 22:14:28 +0200

    add solr annotations

--- src/main/java/com/testCrawler/models/CompanyDocument.java
+++ src/main/java/com/testCrawler/models/CompanyDocument.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.solr.client.solrj.beans.Field;
 
 import java.util.List;
 
@@ -12,8 +13,18 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CompanyDocument {
+    @Field
     private String id;
+
+    @Field
     private List<String> phoneData;
+
+    @Field
     private List<String> socialsData;
+
+    @Field
     private List<String> addressData;
+
+    @Field
+    private boolean fromCrawl;
 }
