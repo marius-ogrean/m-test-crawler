57971f498a2452eef5001c6b333e5e9f5ef476b2
Marius <marius_ogrean@yahoo.com>
2026-03-27 17:33:17 +0200

    update script

--- run_crawler_docker.cmd
+++ run_crawler_docker.cmd
@@ -1,7 +1,9 @@
+set location=%cd%
+
 docker run --network m-test-crawler_default -it ^
 --rm ^
--v %~dp0"/crawler-conf.yaml:/apache-storm/crawler-conf.yaml" ^
--v %~dp0"/crawler.flux:/apache-storm/crawler.flux" ^
--v %~dp0"/target/stormcrawler-1.0-SNAPSHOT.jar:/apache-storm/stormcrawler-1.0-SNAPSHOT.jar" ^
+-v "D:\\Development\\m-test-crawler\\crawler-conf.yaml:/apache-storm/crawler-conf.yaml" ^
+-v "D:\\Development\\m-test-crawler\\crawler.flux:/apache-storm/crawler.flux" ^
+-v "D:\\Development\\m-test-crawler\\target\\stormcrawler-1.0-SNAPSHOT.jar:/apache-storm/stormcrawler-1.0-SNAPSHOT.jar" ^
 storm:latest ^
-storm jar target/stormcrawler-1.0-SNAPSHOT.jar org.apache.storm.flux.Flux --remote crawler.flux
+storm jar /apache-storm/stormcrawler-1.0-SNAPSHOT.jar org.apache.storm.flux.Flux --remote crawler.flux
