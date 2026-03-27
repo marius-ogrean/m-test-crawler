d513e6e63ef99ff4666c189716088261d38f89e1
Marius <marius_ogrean@yahoo.com>
2026-03-27 15:10:16 +0200

    refactor

--- src/main/java/com/testCrawler/indexing/WholeTupleIndexer.java
+++ src/main/java/com/testCrawler/indexing/WholeTupleIndexer.java
@@ -3,7 +3,7 @@ package com.testCrawler.indexing;
 import org.apache.storm.tuple.Tuple;
 import org.apache.stormcrawler.indexing.AbstractIndexerBolt;
 
-public class WholeTupleIndexer  extends AbstractIndexerBolt {
+public class WholeTupleIndexer extends AbstractIndexerBolt {
     @Override
     public void execute(Tuple tuple) {
         System.out.println("tuple:");
